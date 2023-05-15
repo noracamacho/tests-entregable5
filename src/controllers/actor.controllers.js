80daa642079c34f45483bbc05c3aa5030cc34bec
noracamacho <ingnorac@gmail.com>
2023-05-14 23:49:03 -0600

    first commit

--- src/controllers/actor.controllers.js
+++ src/controllers/actor.controllers.js
@@ -2,7 +2,7 @@ const catchError = require('../utils/catchError');
 const Actor = require('../models/Actor');
 
 const getAll = catchError(async(req, res) => {
-    const results = await Actor.findAll();
+    const results = await Actor.findAll({ order: ['firstName']});
     return res.json(results);
 });
 
