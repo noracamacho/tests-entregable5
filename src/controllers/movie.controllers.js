976b2f596bd10f013f14040933c3c61f05cea71a
noracamacho <ingnorac@gmail.com>
2023-05-15 01:27:39 -0600

    first commit

--- src/controllers/movie.controllers.js
+++ src/controllers/movie.controllers.js
@@ -5,7 +5,7 @@ const Actor = require('../models/Actor');
 const Director = require('../models/Director');
 
 const getAll = catchError(async(req, res) => {
-    const results = await Movie.findAll({ include: [Genre, Actor, Director] });
+    const results = await Movie.findAll({ order: ['name'] ,include: [Genre, Actor, Director] });
     return res.json(results);
 });
 
